3e11e6eba75c49d5a755180180cf2a9b3f1a1583
graphemecluster <graphemecluster@gmail.com>
2023-10-03 16:07:43 +0800

    Eliminate `var`

--- gratex.js
+++ gratex.js
@@ -1,13 +1,13 @@
-var calcElt = document.getElementById('calculator');
-var calculator2D = Desmos.GraphingCalculator(calcElt, {
+const calcElt = document.getElementById('calculator');
+const calculator2D = Desmos.GraphingCalculator(calcElt, {
     border: false,
     pasteGraphLink: true
 });
 calculator2D.setExpression({ latex: 'x^2+y^2=10' });
 
-var calcLabelElt = document.getElementById('calculator-label');
+const calcLabelElt = document.getElementById('calculator-label');
 calcLabelElt.style.display = 'none';
-var calculatorLabel = Desmos.GraphingCalculator(calcLabelElt, {
+const calculatorLabel = Desmos.GraphingCalculator(calcLabelElt, {
     border: false,
     graphpaper: false,
     settingsMenu: false,
@@ -18,33 +18,33 @@ var calculatorLabel = Desmos.GraphingCalculator(calcLabelElt, {
     actions: false,
     branding: false
 });
-var calculatorLabelScreenshot = Desmos.GraphingCalculator(document.createElement('div'), {
+const calculatorLabelScreenshot = Desmos.GraphingCalculator(document.createElement('div'), {
     showGrid: false,
     showXAxis: false,
     showYAxis: false
 });
 
-var btnElt = document.getElementById('screenshot-button');
+const btnElt = document.getElementById('screenshot-button');
 btnElt.addEventListener('click', generate);
 
-var btnImp = document.getElementById('import-button');
+const btnImp = document.getElementById('import-button');
 btnImp.addEventListener('click', () => importGraph(desmosHash.value));
 
-var containerElt = document.getElementById('generate-container');
+const containerElt = document.getElementById('generate-container');
 
-var preview = document.getElementById('preview');
-var download = document.getElementById('downloadButton');
-var labelSize = document.forms.labelSize.elements[0];
-var imageDimension = document.forms.imageDimension.elements[0];
+const preview = document.getElementById('preview');
+const download = document.getElementById('downloadButton');
+const labelSize = document.forms.labelSize.elements[0];
+const imageDimension = document.forms.imageDimension.elements[0];
 
-var color = document.getElementById('color');
-var widegraph = document.getElementById('widegraph');
-var credit = document.getElementById('credit');
-var hideLaTeX = document.getElementById('hideLaTeX');
-var desmosHash = document.getElementById('desmos-hash');
+const color = document.getElementById('color');
+const widegraph = document.getElementById('widegraph');
+const credit = document.getElementById('credit');
+const hideLaTeX = document.getElementById('hideLaTeX');
+const desmosHash = document.getElementById('desmos-hash');
 
 window.onload = () => {
-    var q = getUrlQueries();
+    const q = getUrlQueries();
     if (q['widegraph'] || q['wide'] || q['w']) widegraph.checked = true;
     if (q['addcredit'] || q['credit'] || q['c']) credit.checked = true;
     if (q['hidelatex'] || q['hide'] || q['h']) hideLaTeX.checked = true;
@@ -56,33 +56,33 @@ window.onload = () => {
     importGraph(q['url'] || q['hash']);
 };
 
-var calc3DElt = document.getElementById('calculator-3d');
+const calc3DElt = document.getElementById('calculator-3d');
 calc3DElt.style.display = 'none';
 document.querySelectorAll('input[name="version"]').forEach(element => {
     element.addEventListener('change', event => {
-        var is2D = event.target.value === 'version-2d';
+        const is2D = event.target.value === 'version-2d';
         calcElt.style.display = is2D ? '' : 'none';
         calc3DElt.style.display = is2D ? 'none' : '';
     });
 });
 
-var calculator3D;
+let calculator3D;
 calc3DElt.onload = () => {
     calculator3D = calc3DElt.contentWindow.Calc;
 };
 
-var canvas = document.createElement('canvas');
-var context = canvas.getContext('2d');
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
 
 function generate() {
-    var graphImg = new Image();
-    var mergeImg = new Image();
-    var [width, height, graphSize, graphMargin, labelPos] = imageDimension.value.split(',').map(num => +num);
-    var doubleGraphSize = graphSize << 1;
+    const graphImg = new Image();
+    const mergeImg = new Image();
+    const [width, height, graphSize, graphMargin, labelPos] = imageDimension.value.split(',').map(num => +num);
+    const doubleGraphSize = graphSize << 1;
     canvas.width = width;
     canvas.height = height;
 
-    var is2D = document.querySelector('input[name="version"]:checked').value === 'version-2d';
+    const is2D = document.querySelector('input[name="version"]:checked').value === 'version-2d';
     Promise.all([
         new Promise(resolve => graphImg.onload = resolve),
         new Promise(resolve => mergeImg.onload = resolve)
@@ -90,8 +90,8 @@ function generate() {
         context.fillStyle = color.value;
         context.fillRect(0, 0, width, height);
 
-        var invertGraph = is2D && calculator2D.graphSettings.invertedColors;
-        var invertLabel = contrast(color.value) === 'white';
+        const invertGraph = is2D && calculator2D.graphSettings.invertedColors;
+        const invertLabel = contrast(color.value) === 'white';
         if (invertGraph) reverse();
 
         if (widegraph.checked) context.drawImage(graphImg, (width - doubleGraphSize) >> 1, graphMargin, doubleGraphSize, graphSize);
@@ -115,7 +115,7 @@ function generate() {
         containerElt.style.display = 'block';
     });
 
-    var calculator = is2D ? calculator2D : calculator3D;
+    const calculator = is2D ? calculator2D : calculator3D;
     calculator.asyncScreenshot({
         showLabels: true,
         width: 320 * (widegraph.checked + 1),
@@ -123,7 +123,7 @@ function generate() {
         targetPixelRatio: graphSize / 320
     }, s => graphImg.src = s);
 
-    var ratio = (Math.min(width, height) >= 360) + 1;
+    const ratio = (Math.min(width, height) >= 360) + 1;
     calculatorLabelScreenshot.setExpression({
         id: 'label',
         latex: '\\left(0,-' + labelPos + '\\right)',
@@ -161,14 +161,14 @@ function getUrlQueries() {
 
 function importGraph(hash) {
     if (hash) {
-        var match = /^\s*(?:https?:\/\/)?(?:[-a-zA-Z0-9]*\.)?desmos\.com(?::[0-9]+)?\/(calculator|3d)\/([^?#\/\s]+)/.exec(hash);
+        const match = /^\s*(?:https?:\/\/)?(?:[-a-zA-Z0-9]*\.)?desmos\.com(?::[0-9]+)?\/(calculator|3d)\/([^?#\/\s]+)/.exec(hash);
         if (match) loadGraph(match[2], match[1] === 'calculator');
         else loadGraph(hash, document.querySelector('input[name="version"]:checked').value === 'version-2d');
     }
 }
 
 function loadGraph(hash, is2D) {
-    var url = is2D
+    let url = is2D
         ? 'https://saved-work.desmos.com/calc-states/production/'
         : 'https://saved-work.desmos.com/calc-3d-states/production/';
     url += hash;
@@ -192,19 +192,19 @@ function getLabel(calculator) {
         case 'hide':
             return '?????????';
         case 'top-expression':
-            var exp = calculator.getExpressions().find(exp => exp.latex);
+            const exp = calculator.getExpressions().find(exp => exp.latex);
             return exp ? exp.latex : '?????????';
         case 'custom':
-            var exps = calculatorLabel.getExpressions().flatMap(exp => exp.latex ? [`\\textcolor{black}{${exp.latex}}`] : '');
-            var spacing = Math.max(Math.ceil(Math.log2(exps.length)) - 1, 1);
+            const exps = calculatorLabel.getExpressions().flatMap(exp => exp.latex ? [`\\textcolor{black}{${exp.latex}}`] : '');
+            const spacing = Math.max(Math.ceil(Math.log2(exps.length)) - 1, 1);
             return exps.length ? `\\textcolor{transparent}{${groupLines(exps, spacing)}}` : '?????????';
     }
 }
 
 // https://github.com/FuriousChocolate/LaTeXmos/blob/main/website/convert.js
 function groupLines(lines, n) {
-    var newLines = [];
-    for (var i = 0; i < lines.length - 1; i += 2) {
+    const newLines = [];
+    for (let i = 0; i < lines.length - 1; i += 2) {
         newLines.push(`\\binom{${lines[i]}}{${nestOverline(lines[i + 1], n * 3)}}`);
     }
     if (lines.length & 1) {
